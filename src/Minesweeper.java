76d2964134137720a89930be1dcc95426eec9a7c
esmahaber <l1621012063@stud.sdu.edu.tr>
2020-10-08 15:39:50 +0300

    First commit

---
+++ src/Minesweeper.java
@@ -0,0 +1,29 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ACER
+ */
+public class Minesweeper {
+
+    int rowNumber, colNumber;
+    int[][] map;
+    int[][] board;
+
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    public Minesweeper(int rowNumber, int colNumber) {
+        this.rowNumber = rowNumber;
+        this.colNumber = colNumber;
+        this.map = new int[rowNumber][colNumber];
+        this.board = new int[rowNumber][colNumber];;
+    }
+}
