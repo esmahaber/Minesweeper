b6b43281d407105e624ca4f634b94fea9ab43ea3
esmahaber <l1621012063@stud.sdu.edu.tr>
2020-10-13 15:07:33 +0300

    random mine generated

--- src/minesweeperGUI/Minesweeper.java
+++ src/minesweeperGUI/Minesweeper.java
@@ -6,6 +6,8 @@
 package minesweeperGUI;
 
 import java.awt.GridLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 /**
@@ -16,22 +18,41 @@ public class Minesweeper {
 
     JFrame frame;
     Button[][] board = new Button[10][10];
+    ImageIcon imageIcon;
     
     public Minesweeper() {
         frame = new JFrame("Mayın tarlası");
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(10, 10));
-        
+
         for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board.length; col++) {
+            for (int col = 0; col < board[0].length; col++) {
                 Button button = new Button(row, col);
                 frame.add(button);
                 board[row][col] = button;
-                
-            }            
+
+            }
         }
+
+        generateMine();
         frame.setVisible(true);
-        
     }
+
+    public void generateMine() {
+        int i = 0;
+        while (i < 10) {
+            int randomRow = (int) (Math.random() * board.length);
+            int randomCol = (int) (Math.random() * board[0].length);
+
+            while (board[randomRow][randomCol].isMine()) {
+                randomRow = (int) (Math.random() * board.length);
+                randomCol = (int) (Math.random() * board[0].length);
+            }
+            board[randomRow][randomCol].setMine(true);
+            i++;
+        }
+    }
+
+    
 }
